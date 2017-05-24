b6aa2b21a15eadde975060c08a230f3974955498
bilalakmalcp <bilal.akmal@codingpixel.com>
2017-05-24 19:58:34 +0500

    refresher added

--- src/pages/chat/chat.module.ts
+++ src/pages/chat/chat.module.ts
@@ -14,6 +14,6 @@ import { Chat } from './chat';
   ]
 })
 export class ChatModule {
-  public constructor(){
+  public constructor() {
   }
 }
--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -4,11 +4,11 @@ import { IonicPage, NavController, ModalController, NavParams } from 'ionic-angu
  * Services
  */
 import { UtilityMethods } from '../../services/utility_methods';
-import {ChatService} from "../../services/chat.service";
-import {ChatMessage} from "../../models/ChatMessage";
-import {User} from "../../models/user";
+import { ChatService } from "../../services/chat.service";
+import { ChatMessage } from "../../models/ChatMessage";
+import { User } from "../../models/user";
 
-declare var io:any;
+declare var io: any;
 
 @IonicPage()
 @Component({
@@ -21,40 +21,47 @@ export class Chat {
    * Variables && Configs
    */
   public reply_box_on: boolean;
-  public socket:any;
-  public conversation:Array<ChatMessage> = [];
-  public textMessage:string = "";
-  public secondUser:User = null;
-  public loggedInUser:User = null;
+  public socket: any;
+  public conversation: Array<ChatMessage> = [];
+  public textMessage: string = "";
+  public secondUser: User = null;
+  public loggedInUser: User = null;
 
   /**
    * Constructor
    */
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public utilityMethods: UtilityMethods, public chatService:ChatService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public utilityMethods: UtilityMethods, public chatService: ChatService) {
     this.reply_box_on = false;
-    this.secondUser = new User(3,'bilal','akmal','bilal@gmail.com',null);
-    this.loggedInUser = new User(2,'noman','tufail','noman@gmail.com',null);
+    this.secondUser = new User(3, 'bilal', 'akmal', 'bilal@gmail.com', null);
+    this.loggedInUser = new User(2, 'noman', 'tufail', 'noman@gmail.com', null);
     this.chatService.threadingUser = this.secondUser;
     this.connectionToSocket();
     this.chatService.listenForGlobalMessages();
   }
 
-  public connectionToSocket(){
+  public connectionToSocket() {
     this.socket = io(this.chatService.socketUrl);
-    this.socket.on('receive_message', (msg:any)=>{
-      if(msg.receiverId == this.getLoggedInUserId() && msg.senderId == this.secondUser.id){
-        this.conversation.push(new ChatMessage(1,this.secondUser.full_name,"06:00", msg.message));
+    this.socket.on('receive_message', (msg: any) => {
+      if (msg.receiverId == this.getLoggedInUserId() && msg.senderId == this.secondUser.id) {
+        this.conversation.push(new ChatMessage(1, this.secondUser.full_name, "06:00", msg.message));
       }
     });
   }
 
-  public getLoggedInUserId(){
+  public getLoggedInUserId() {
     return this.loggedInUser.id;
   }
 
-  public sendMessage(){
-    if(this.textMessage != ""){
-      this.socket.emit('send_message',this.textMessage,this.loggedInUser.id, this.secondUser.id);
+  doRefresh(refresher) {
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+  public sendMessage() {
+    if (this.textMessage != "") {
+      this.socket.emit('send_message', this.textMessage, this.loggedInUser.id, this.secondUser.id);
       this.textMessage = "";
     }
   }
@@ -63,9 +70,9 @@ export class Chat {
   * View LifeCycle Events
   */
 
-  ionViewDidLoad() {}
+  ionViewDidLoad() { }
 
-  ionViewWillLeave() {}
+  ionViewWillLeave() { }
 
   /**
   * Methods
@@ -80,7 +87,7 @@ export class Chat {
     this.reply_box_on = true;
   }
 
-  ionViewDidLeave(){
+  ionViewDidLeave() {
     this.chatService.threadingUser = null;
   }
 }
