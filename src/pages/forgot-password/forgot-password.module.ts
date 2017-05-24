f76213ed3129ae02cd11fc180c6320b3efe9589a
bilalakmalcp <bilal.akmal@codingpixel.com>
2017-05-24 10:40:32 +0500

    fix

--- src/pages/forgot-password/forgot-password.module.ts
+++ src/pages/forgot-password/forgot-password.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { Login } from './login';
+import { ForgotPassword } from './forgot-password';
 
 @NgModule({
   declarations: [
-    Login,
+    ForgotPassword,
   ],
   imports: [
-    IonicPageModule.forChild(Login),
+    IonicPageModule.forChild(ForgotPassword),
   ],
   exports: [
-    Login
+    ForgotPassword
   ]
 })
-export class LoginModule {}
+export class ForgotPasswordModule {}
