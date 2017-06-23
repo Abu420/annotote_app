ac4a443552686b71a330412ed97a9b4f9477385f
bilalakmalcp <bilal.akmal@codingpixel.com>
2017-06-23 12:16:53 +0500

    editor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       // let status bar overlay webview
-      this.statusBar.overlaysWebView(false);
+      this.statusBar.overlaysWebView(true);
 
       // set status bar to white
       this.statusBar.backgroundColorByHexString('#000000');
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -181,5 +181,4 @@ export class Home {
     });
     settingsModal.present();
   }
-
 }
