73cb55b659e9393712c87d742e0941e61033d6a7
agha ali abbas <agha.ali@codingpixel.com>
2017-09-18 18:58:39 +0500

    some design issues fixed

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -338,7 +338,11 @@ export class Search {
     }
 
     go_to_browser(anotote) {
-        this.navCtrl.push(AnototeEditor, { ANOTOTE: anotote, FROM: 'search', WHICH_STREAM: 'me' });
+        console.log(anotote);
+        if (anotote.userAnnotote.anototeType == 'me')
+            this.navCtrl.push(AnototeEditor, { ANOTOTE: anotote, FROM: 'search', WHICH_STREAM: anotote.userAnnotote.anototeType, actual_stream: anotote.userAnnotote.anototeType });
+        else
+            this.navCtrl.push(AnototeEditor, { ANOTOTE: anotote, FROM: 'search', WHICH_STREAM: 'anon', actual_stream: 'anon' });
         this.dismiss()
     }
 
